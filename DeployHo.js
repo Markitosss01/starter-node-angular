e887f2fefc9cba3d224e94faaadec4de4df2bc85
Markitosss01 <markitostapia01@gmail.com>
2021-09-11 11:07:21 -0700

    Hands On 2

--- DeployHo.js
+++ DeployHo.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '54.226.6.127',
+      host: '54.225.38.111',
       username: 'ubuntu',
       privateKey: 'ho-key.pem'
     })
